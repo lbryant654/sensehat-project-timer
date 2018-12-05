a09dd14abc2d7cc50690b71705fc37ad2535aaa8
lbryant654 <45632266+lbryant654@users.noreply.github.com>
2018-12-05 09:05:41 -0600

    Add files via upload

--- timerrrrrrrrrrrrrrrrr.py
+++ timerrrrrrrrrrrrrrrrr.py
@@ -3,7 +3,19 @@ from time import *
 sense = SenseHat()
 
 g = [0, 255, 0]
+r = [255, 0, 0]
+x = [0, 0, 0]
+secs = 35
 
-for i in range(5, -1, -1):
-    sense.show_letter(str(i), g)
+for i in range(64):
+    if i < s:
+        timer.append(g)
+    else:
+        timer.append(x)
+
+sense.set_pixels(timer)
+
+for i in range(0, secs):
     sleep(1)
+    timer[i] = r
+    sense.set_pixels(timer)
